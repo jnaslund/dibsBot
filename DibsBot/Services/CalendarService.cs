5643370460983e97f85c2b0f9009b111bba5c05a
Jonas Naslund <jonas.naslund@gmail.com>
2017-11-12 14:15:10 -0600

    Made placeholder email that allows setting orgainzer as optional configurable via appsettings.

--- DibsBot/Services/CalendarService.cs
+++ DibsBot/Services/CalendarService.cs
@@ -6,51 +6,43 @@ using System.Threading.Tasks;
 using System.Web;
 using DibsBot.Models;
 using Microsoft.Bot.Builder.History;
+using System.Web.Configuration;
 
 namespace DibsBot.Services
 {
     public class CalendarService
     {
-        private GraphServiceClient _graphClient = null;
+        private readonly GraphServiceClient _graphClient = null;
 
         public CalendarService(GraphServiceClient graphClient)
         {
             _graphClient = graphClient;
         }
+
         public async Task<MeetingTimeSuggestionsResult> FindMeetingSuggestion(FindRoomRequest request)
         {
+
+            List<Attendee> attendees = new List<Attendee>();
             // Adding placeholder user to be able to remove the
             // Required flag on the user looking for available rooms.
-            var attendees = new List<Attendee>()
+            // An empty collection causes findMeetingTimes to look for free time slots for only the organizer.
+            if (!string.IsNullOrEmpty(WebConfigurationManager.AppSettings["PlaceholderAttendeeEmail"]))
             {
-                new Attendee()
+                attendees = new List<Attendee>()
                 {
-                    EmailAddress = new EmailAddress() { Address = "work@makingwaves.com" }
-                }
-            };
+                    new Attendee()
+                    {
+                        EmailAddress = new EmailAddress()
+                        {
+                            Address = WebConfigurationManager.AppSettings["PlaceholderAttendeeEmail"]
+                        }
+                    }
+                };
+            }
             var locationConstraint = new LocationConstraint()
             {
                 IsRequired = true,
                 SuggestLocation = true,
-                //Locations = new List<LocationConstraintItem>()
-                //{
-                //    new LocationConstraintItem()
-                //    {
-                //        LocationEmailAddress = "roomchi-library@makingwaves.com"
-                //    },
-                //    new LocationConstraintItem()
-                //    {
-                //        LocationEmailAddress = "roomchi-pink@makingwaves.com"
-                //    },
-                //    new LocationConstraintItem()
-                //    {
-                //        LocationEmailAddress = "roomchi-cabin@makingwaves.com"
-                //    },
-                //    new LocationConstraintItem()
-                //    {
-                //        LocationEmailAddress = "roomchi-fishbowl@makingwaves.com"
-                //    }
-                //}
             };
             var timeConstraint = new TimeConstraint()
             {
@@ -73,7 +65,7 @@ namespace DibsBot.Services
                 }
 
             };
-            return await _graphClient.Me.FindMeetingTimes(Attendees: attendees, TimeConstraint: timeConstraint, MeetingDuration: new Duration(request.Duration), IsOrganizerOptional: true, LocationConstraint: locationConstraint)
+            return await _graphClient.Me.FindMeetingTimes(attendees, TimeConstraint: timeConstraint, MeetingDuration: new Duration(request.Duration), IsOrganizerOptional: attendees.Any(), LocationConstraint: locationConstraint)
                 .Request()
                 .PostAsync();
         }
